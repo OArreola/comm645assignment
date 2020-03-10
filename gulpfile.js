1e6487f6435167a89530a93b1e286658e444adda
OArreola <orlando_arreola@hotmail.com>
2020-03-10 12:32:31 -0700

    Added minifying of css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-var { src, dest, watch } = require("gulp");
-var sass = require("gulp-sass");
+let { src, dest, watch } = require("gulp");
+let sass = require("gulp-sass");
+let cleanCSS = require("gulp-clean-css");
 sass.compiler = require("node-sass");
 
 function css() {
   return src("src/*.scss")
     .pipe(sass())
+    .pipe(cleanCSS({ compatibility: "ie8" }))
     .pipe(dest("dist/css"));
 }
 
